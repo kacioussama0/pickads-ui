559ce8e727dcca5b8894599b51c68a311ca7c58e
unknown <kacioussama0@gmail.com>
2023-10-13 19:54:05 +0100

    some-update

--- src/views/Influencers.vue
+++ src/views/Influencers.vue
@@ -284,7 +284,7 @@ export default  {
       </div>
 
 
-    <div class="row g-4 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-5 my-3">
+    <div class="row g-4 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5 my-3">
 
       <div  v-for="influencer in getInfluncers.data" v-if="influencersLoaded" >
 
